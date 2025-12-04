5dd6e598b7d451b10b572cd713e01f66ccb0fc82
Siva Movva <smnitro555@gmail.com>
2025-12-04 10:26:15 -0500

    More documentation on brainstorming

--- repository_structure_requirements.md
+++ repository_structure_requirements.md
@@ -5,7 +5,46 @@ This file is meant to highlight software capability and requirements of this rep
 ## Code Architecture
 
 - `docs`
+    - Location for sphinx documentation that highlights the following topics:
+        - Changelog
+        - GPIO Pins for Raspi Setup
+            - Explain which pins for Temperature and Humidity sensor
+            - 
+            - (Vector graphic images)
+        - Software architecture diagram
+        - Sphinx Auto-Doc of Python code base
 - `src`
+    - `raspi`
+        - `greenhouse_driver_start.sh`
+            - Script that Raspberry Pi will point to on boot up
+                - Ensures packages are updated
+                - Pulls the latest version from Github
+                - Starts the `greenhouse_manager.py`
+                - Starts hosting the webserver
+    - `greenhouse_manager`
+        - `greenhouse_manager.py`
+            - Main script that will run continuously
+            - Settings should be read from a pydantic JSON file
+            - Logic should continously do the following
+                - Continously
+                    - Check if settings file has been updated
+                    - Monitor Temperature and Humidity
+                    - Adjust heater and vent fan accordingly
+                    - Based on time schedule turn on/off grow lights
+                    - Based on time schedule turn on/off
+                    - Based on time schedule take images of greenhouse
+                    - Write state information (date, 24hr time, greenhouse temp, greenhouse humidity, greenhouse pressure, heater state, vent state, grow lights, stand fan) to log files
+                        - One log file per day ideally
+                        - Pandas DataFrame for each day
+                        - (binary format to save space)
+                - On Interupt from GPIO Button
+                    - Turn on/off the device
+                    - Illuminate the button
+    - `webserver`
+        - Utilize flask to generate the website (Should be dynamic, allowing to refresh on new data that is being written)
+        - Code related to hosting a Plotly dashboard of time vs. remaining parameters (X axis on all plots is datetime (default zoom is for the current day, but allow for week, month type views))
+        - Code related to hosting a image slideshow with a time slider to see time-lapse of greenhouse state
+
 - `test`
 
 ## Development Operations
@@ -20,6 +59,7 @@ This file is meant to highlight software capability and requirements of this rep
         - `run-webserver` to host the webserver 
 - Pytest framework (also utilizing doctest)
     - Allow mock testing of GPIO capability
+    - Use doctest to check public methods
 - Virtual environment is built with uv
     - Support building on both windows machine (for testing/debugging)
     - Support building on linux machine (deployed Raspberry Pi)
--- src/driver.py
+++ src/driver.py
@@ -3,8 +3,8 @@ import os
 
 # Set environment variable for GPIOZERO to use a mock library for development
 # on non-Raspberry Pi machines. This must be done before importing RPi.GPIO.
-if os.name != 'posix':
-    os.environ['GPIOZERO_PIN_FACTORY'] = 'mock'
+if os.name != "posix":
+    os.environ["GPIOZERO_PIN_FACTORY"] = "mock"
 
 import RPi.GPIO as GPIO  # https://pypi.org/project/RPi.GPIO/
 import bme280  # https://pypi.org/project/RPi.bme280/
@@ -22,8 +22,8 @@ HUMIDITY_MARGIN = 5
 DESIRED_TEMP = 66
 TEMP_MARGIN = 4
 
-NIGHTLIGHT_ON = 'time'
-NIGHTLIGHT_OFF = 'time'
+NIGHTLIGHT_ON = "time"
+NIGHTLIGHT_OFF = "time"
 CIRC_FAN_MINS_PER_HOUR = 5
 AVERAGE_INTERVAL = 5  # Minutes
 SWITCH_TIMEOUT = 5 * 60  # Minutes * Seconds/Min
@@ -39,53 +39,55 @@ class Greenhouse:
         self.current_humidity = None
         self.average_temp = None
         self.average_humidity = None
-        self.switch_status = {'VentFan': None,
-                              'Heater': None,
-                              'GrowLight': None,
-                              'StandFan': None}
+        self.switch_status = {
+            "VentFan": None,
+            "Heater": None,
+            "GrowLight": None,
+            "StandFan": None,
+        }
         self.light_range = [NIGHTLIGHT_OFF, NIGHTLIGHT_ON]
         # Load the RF Keys
-        if os.path.isfile('rf_keys.yaml'):
-            with open('rf_keys.yaml', 'r') as rf_keys_file:
+        if os.path.isfile("rf_keys.yaml"):
+            with open("rf_keys.yaml", "r") as rf_keys_file:
                 self.rf_transmit_keys = yaml.load(rf_keys_file)
         else:
-            raise FileNotFoundError('Cannot find rf_keys.yaml file')
+            raise FileNotFoundError("Cannot find rf_keys.yaml file")
 
     def vent_fan_callback(self, channel):
-        print('Vent Fan Button Press Detected')
-        if self.switch_status['VentFan'] is False:
-            print('Engaging Vent Fan')
-            self.switch_on('VentFan')
+        print("Vent Fan Button Press Detected")
+        if self.switch_status["VentFan"] is False:
+            print("Engaging Vent Fan")
+            self.switch_on("VentFan")
         else:
-            print('Powering Down Vent Fan')
-            self.switch_off('VentFan')
+            print("Powering Down Vent Fan")
+            self.switch_off("VentFan")
 
     def heater_callback(self, channel):
-        print('Heater Button Press Detected')
-        if self.switch_status['Heater'] is False:
-            print('Engaging Heater')
-            self.switch_on('Heater')
+        print("Heater Button Press Detected")
+        if self.switch_status["Heater"] is False:
+            print("Engaging Heater")
+            self.switch_on("Heater")
         else:
-            print('Powering Down Heater')
-            self.switch_off('Heater')
+            print("Powering Down Heater")
+            self.switch_off("Heater")
 
     def grow_light_callback(self, channel):
-        print('Grow Light Button Press Detected')
-        if self.switch_status['GrowLight'] is False:
-            print('Engaging Grow Light')
-            self.switch_on('GrowLight')
+        print("Grow Light Button Press Detected")
+        if self.switch_status["GrowLight"] is False:
+            print("Engaging Grow Light")
+            self.switch_on("GrowLight")
         else:
-            print('Powering Down GrowLight')
-            self.switch_off('GrowLight')
+            print("Powering Down GrowLight")
+            self.switch_off("GrowLight")
 
     def stand_fan_callback(self, channel):
-        print('Stand Fan Button Press Detected')
-        if self.switch_status['StandFan'] is False:
-            print('Engaging Stand Fan')
-            self.switch_on('StandFan')
+        print("Stand Fan Button Press Detected")
+        if self.switch_status["StandFan"] is False:
+            print("Engaging Stand Fan")
+            self.switch_on("StandFan")
         else:
-            print('Powering Down Stand Fan')
-            self.switch_off('StandFan')
+            print("Powering Down Stand Fan")
+            self.switch_off("StandFan")
 
     def initialize(self):
         # Turn Everything off
@@ -94,38 +96,62 @@ class Greenhouse:
         # Initialize Buttons
         for key in self.rf_transmit_keys.keys():
             # Setup Button
-            GPIO.setup(self.rf_transmit_keys[key]['ButtonGPIO'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(
+                self.rf_transmit_keys[key]["ButtonGPIO"],
+                GPIO.IN,
+                pull_up_down=GPIO.PUD_UP,
+            )
             # Setup LED
-            GPIO.setup(self.rf_transmit_keys[key]['LEDGPIO'], GPIO.OUT)
-            GPIO.output(self.rf_transmit_keys[key]['LEDGPIO'], GPIO.HIGH)
-            GPIO.output(self.rf_transmit_keys[key]['LEDGPIO'], GPIO.LOW)
+            GPIO.setup(self.rf_transmit_keys[key]["LEDGPIO"], GPIO.OUT)
+            GPIO.output(self.rf_transmit_keys[key]["LEDGPIO"], GPIO.HIGH)
+            GPIO.output(self.rf_transmit_keys[key]["LEDGPIO"], GPIO.LOW)
         # Set Interrupts for Button Presses
-        GPIO.add_event_detect(self.rf_transmit_keys['VentFan']['ButtonGPIO'], GPIO.FALLING,
-                              callback=self.vent_fan_callback, bouncetime=300)
-        GPIO.add_event_detect(self.rf_transmit_keys['Heater']['ButtonGPIO'], GPIO.FALLING,
-                              callback=self.heater_callback, bouncetime=300)
-        GPIO.add_event_detect(self.rf_transmit_keys['GrowLight']['ButtonGPIO'], GPIO.FALLING,
-                              callback=self.grow_light_callback, bouncetime=300)
-        GPIO.add_event_detect(self.rf_transmit_keys['StandFan']['ButtonGPIO'], GPIO.FALLING,
-                              callback=self.stand_fan_callback, bouncetime=300)
+        GPIO.add_event_detect(
+            self.rf_transmit_keys["VentFan"]["ButtonGPIO"],
+            GPIO.FALLING,
+            callback=self.vent_fan_callback,
+            bouncetime=300,
+        )
+        GPIO.add_event_detect(
+            self.rf_transmit_keys["Heater"]["ButtonGPIO"],
+            GPIO.FALLING,
+            callback=self.heater_callback,
+            bouncetime=300,
+        )
+        GPIO.add_event_detect(
+            self.rf_transmit_keys["GrowLight"]["ButtonGPIO"],
+            GPIO.FALLING,
+            callback=self.grow_light_callback,
+            bouncetime=300,
+        )
+        GPIO.add_event_detect(
+            self.rf_transmit_keys["StandFan"]["ButtonGPIO"],
+            GPIO.FALLING,
+            callback=self.stand_fan_callback,
+            bouncetime=300,
+        )
 
     def switch_off(self, device_name):
-        os.system('../../rfoutlet/codesend %i' % self.rf_transmit_keys[device_name]['SendOff'])
+        os.system(
+            "../../rfoutlet/codesend %i" % self.rf_transmit_keys[device_name]["SendOff"]
+        )
         self.switch_status[device_name] = False
-        GPIO.output(self.rf_transmit_keys[device_name]['LEDGPIO'], GPIO.LOW)
+        GPIO.output(self.rf_transmit_keys[device_name]["LEDGPIO"], GPIO.LOW)
 
     def switch_on(self, device_name, update_val=time.time()):
-        os.system('../../rfoutlet/codesend %i' % self.rf_transmit_keys[device_name]['SendOn'])
+        os.system(
+            "../../rfoutlet/codesend %i" % self.rf_transmit_keys[device_name]["SendOn"]
+        )
         # Log the time the device is switched on
         self.switch_status[device_name] = update_val
-        GPIO.output(self.rf_transmit_keys[device_name]['LEDGPIO'], GPIO.HIGH)
+        GPIO.output(self.rf_transmit_keys[device_name]["LEDGPIO"], GPIO.HIGH)
 
     def log_conditions(self):
         a = 1
 
     @staticmethod
     def get_condition_filename(date):
-        return 'test'
+        return "test"
 
     def update_switches(self):
         a = 1
@@ -134,10 +160,13 @@ class Greenhouse:
         a = 1
 
     def write_html(self):
-        with open('../index.html', 'w') as html_file:
+        with open("../index.html", "w") as html_file:
             html_file.write(
-                '<h1 style="text-align: center;"><span style="text-decoration: underline;">Greenhouse Dashboard</span></h1>')
-            html_file.write('<p>Date Last Updated: %s</p>' % self.now.strftime("%H:%M:%S"))
+                '<h1 style="text-align: center;"><span style="text-decoration: underline;">Greenhouse Dashboard</span></h1>'
+            )
+            html_file.write(
+                "<p>Date Last Updated: %s</p>" % self.now.strftime("%H:%M:%S")
+            )
             html_file.write(self.generate_plotly_plot)
 
     def main(self):
@@ -145,24 +174,31 @@ class Greenhouse:
         while True:
             # Log Temperature
             # Read Switch Over-ride for Heater
-            if (self.switch_status['Heater'] is not False) and (self.switch_status['Heater'] > 0) and (
-                    time.time() - self.switch_status['Heater'] > SWITCH_TIMEOUT):
-                self.switch_off('Heater')
-            if (self.switch_status['VentFan'] is not False) and (self.switch_status['VentFan'] > 0) and (
-                    time.time() - self.switch_status['VentFan'] > SWITCH_TIMEOUT):
-                self.switch_off('VentFan')
+            if (
+                (self.switch_status["Heater"] is not False)
+                and (self.switch_status["Heater"] > 0)
+                and (time.time() - self.switch_status["Heater"] > SWITCH_TIMEOUT)
+            ):
+                self.switch_off("Heater")
+            if (
+                (self.switch_status["VentFan"] is not False)
+                and (self.switch_status["VentFan"] > 0)
+                and (time.time() - self.switch_status["VentFan"] > SWITCH_TIMEOUT)
+            ):
+                self.switch_off("VentFan")
 
             if self.average_temp < (DESIRED_TEMP + TEMP_MARGIN):
-                self.switch_on('Heater', update_val=-1)
+                self.switch_on("Heater", update_val=-1)
             elif (self.average_temp > (DESIRED_TEMP + TEMP_MARGIN)) or (
-                    self.average_humidity > (DESIRED_HUMIDITY - HUMIDITY_MARGIN)):
-                self.switch_on('VentFan', update_val=-1)
+                self.average_humidity > (DESIRED_HUMIDITY - HUMIDITY_MARGIN)
+            ):
+                self.switch_on("VentFan", update_val=-1)
             else:
                 # Switch VentFan or Heater Off
-                if self.switch_status['Heater'] is not False:
-                    self.switch_off('Heater')
-                if self.switch_status['VentFan'] is not False:
-                    self.switch_off('VentFan')
+                if self.switch_status["Heater"] is not False:
+                    self.switch_off("Heater")
+                if self.switch_status["VentFan"] is not False:
+                    self.switch_off("VentFan")
 
             # Append data to dataframe
             # Generate temperature plot
@@ -170,6 +206,6 @@ class Greenhouse:
             time.sleep(SLEEP_DELAY)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     GREENHOUSE = Greenhouse()
     GREENHOUSE.main()
